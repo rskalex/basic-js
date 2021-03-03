7cb5fcf544a1b417dddf83e56136118de2d3e076
alex <dobrovolya48@gmail.com>
2021-03-03 11:13:41 +0300

    done count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
+    let countCatArr = [];
+
+    matrix.forEach(item => {
+        const count = item.filter(el => {
+            return el === '^^';
+        });
+        countCatArr = countCatArr.concat(count);
+    });
+    return countCatArr.length;
 };
